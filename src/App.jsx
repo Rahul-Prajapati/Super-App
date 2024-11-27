03ca34659194745d3ac03537b5117b601d269e10
Rahul Prajapati <rahul.prajapati1127@gmail.com>
2024-11-27 21:22:09 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import Movies from './pages/Movies'
+
+function App() {
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/movies" element={<Movies />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App
